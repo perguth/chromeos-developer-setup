ac1bf20e81f49752292ce5b0435579e7d4b77b40
Per Guth <mail@perguth.de>
2024-06-20 20:30:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,10 @@
 
 NAME=chromeos-developer-setup
 
-# Clean up
+# Clean up when done and on abort
 trap "cd .. && rm -rf $NAME" EXIT
-  
-# Temporary bugfix as of `02/28/2020`
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
 
 # Install Ansible
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/ansible.list
 sudo apt update && sudo apt install -y ansible
 
 # Clone repository
